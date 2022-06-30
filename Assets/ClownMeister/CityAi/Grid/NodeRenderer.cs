cf9c7be2cb086a1fd886612742a58286511872b8
Lucián Blažek <lucian.blazek@mediafactory.cz>
2022-06-30 17:33:03 +0200

    grid update

---
+++ Assets/ClownMeister/CityAi/Grid/NodeRenderer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace ClownMeister.CityAi.Grid
+{
+    public class NodeRenderer : MonoBehaviour
+    {
+        public void Render(Vector3 position, Node node, float nodeSize)
+        {
+        }
+
+        public void RenderGrid(Vector3 position, NodeGrid grid, float nodeSize, float spacing)
+        {
+        }
+    }
+}
--- Assets/ClownMeister/CityAi/Manager/GridManager.cs
+++ Assets/ClownMeister/CityAi/Manager/GridManager.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 
 namespace ClownMeister.CityAi.Manager
 {
+    [RequireComponent(typeof(NodeRenderer))]
     public class GridManager : MonoBehaviour
     {
         public Vector2Int gridResolution;
+        public Vector3 gridRealPosition;
+        public float gridNodeSize;
+        public float gridNodeSpacing;
 
         private NodeGrid grid;
+        private NodeRenderer renderer;
 
         private void Start()
         {
+            this.renderer = GetComponent<NodeRenderer>();
             this.grid = new NodeGrid(this.gridResolution);
+        }
+
+        private void Render()
+        {
+            this.renderer.RenderGrid(this.gridRealPosition, this.grid, this.gridNodeSize, this.gridNodeSpacing);
         }
     }
 }
